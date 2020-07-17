4d7e8b22a65cead4bc089e3d1c3bbc000798afc3
maruko <azusanosuke@gmail.com>
2020-07-17 23:17:14 +0900

    Add motion

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -3,7 +3,8 @@ import 'package:meta/meta.dart';
 
 import 'model/product.dart';
 
-// TODO: Add velocity constant (104)
+// Add velocity constant (104)
+const double _kFlingVelocity = 2;
 
 class Backdrop extends StatefulWidget {
   final Category currentCategory;
@@ -12,6 +13,7 @@ class Backdrop extends StatefulWidget {
   final Widget frontTitle;
   final Widget backTitle;
 
+  // ignore: sort_constructors_first
   const Backdrop({
     @required this.currentCategory,
     @required this.frontLayer,
@@ -43,7 +45,7 @@ class _FrontLayer extends StatelessWidget {
     return Material(
       elevation: 16,
       shape: const BeveledRectangleBorder(
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(46.0)),
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(46)),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -66,32 +68,90 @@ class _BackdropState extends State<Backdrop>
     with SingleTickerProviderStateMixin {
   final GlobalKey _backdropKey = GlobalKey(debugLabel: 'Backdrop');
 
-  // TODO: Add AnimationController widget (104)
+  // Add AnimationController widget (104)
+  AnimationController _controller;
 
-  // TODO: Add BuildContext and BoxConstraints parameters to _buildStack (104)
-  Widget _buildStack() {
+  // ウィジェットがレンダーツリーの一部になる前に1回だけ呼び出される
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      value: 1,
+      vsync: this,
+    );
+  }
+
+  // TODO: Add override for didUpdateWidget (104)
+
+  // ウィジェットがツリーから完全に削除されたとき1回だけ呼び出される
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // Add functions to get and change front layer visibility (104)
+  bool get _frontLayerVisible {
+    // ignore: omit_local_variable_types
+    final AnimationStatus status = _controller.status;
+    return status == AnimationStatus.completed ||
+        status == AnimationStatus.forward;
+  }
+
+  void _toggleBackdropLayerVisibility() {
+    _controller.fling(
+        velocity: _frontLayerVisible ? -_kFlingVelocity : _kFlingVelocity);
+  }
+
+  // Add BuildContext and BoxConstraints parameters to _buildStack (104)
+  Widget _buildStack(BuildContext context, BoxConstraints constraints) {
+    // ignore: omit_local_variable_types
+    const double layerTitleHeight = 48;
+    final layerSize = constraints.biggest;
+    final layerTop = layerSize.height - layerTitleHeight;
+
+    // TODO: Create a RelativeRectTween Animation (104)
+    Animation<RelativeRect> layerAnimation;
+    layerAnimation = RelativeRectTween(
+      begin: RelativeRect.fromLTRB(
+          0, layerTop, 0, layerTop - layerSize.height),
+      end: const RelativeRect.fromLTRB(0, 0, 0, 0),
+    ).animate(_controller.view);
+
     return Stack(
       key: _backdropKey,
       children: <Widget>[
         // TODO: Wrap backLayer in an ExcludeSemantics widget (104)
-        widget.backLayer,
+        ExcludeSemantics(
+          child: widget.backLayer,
+          excluding: _frontLayerVisible,
+        ),
         // TODO: Add a PositionedTransition (104)
-        // TODO: Wrap front layer in _FrontLayer (104)
-        _FrontLayer(child: widget.frontLayer),
+        PositionedTransition(
+          rect: layerAnimation,
+          child: _FrontLayer(
+            // TODO: Implement onTap property on _BackdropState (104)
+            child: widget.frontLayer,
+          ),
+        ),
       ],
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    var appBar = AppBar(
+    final appBar = AppBar(
       brightness: Brightness.light,
       elevation: 0,
       titleSpacing: 0,
         // TODO: Replace leading menu icon with IconButton (104)
         // TODO: Remove leading property (104)
         // TODO: Create title with _BackdropTitle parameter (104)
-      leading: Icon(Icons.menu),
+      leading: IconButton(
+        icon: Icon(Icons.menu),
+        onPressed: _toggleBackdropLayerVisibility,
+      ),
       title: Text('SHRINE'),
       actions: <Widget>[
         // TODO: Add shortcut to login screen from trailing icons (104)
@@ -118,7 +178,7 @@ class _BackdropState extends State<Backdrop>
     return Scaffold(
       appBar: appBar,
       // TODO: Return a LayoutBuilder widget (104)
-      body: _buildStack(),
+      body: LayoutBuilder(builder: _buildStack),
     );
   }
 }
